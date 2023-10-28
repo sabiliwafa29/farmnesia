ab821fe4224a3d2ab9fafb92f06789c008843a9d
sabiliwafa29 <rosyadsiwa04@gmail.com>
2023-10-29 01:22:17 +0700

    sda

--- lib/pages/ble_connect.dart
+++ lib/pages/ble_connect.dart
@@ -1,31 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(Ble_connect());
 }
 class Ble_connect extends StatelessWidget{
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BLE Connection'),
-        centerTitle: true,
+        title: Text(
+          'BLE Connection',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            // fontFamily: 'Poppins',
+            fontWeight: FontWeight.w600,
+            height: 0,
+          ),
+        ),
       ),
-      
-
-      body: Column(
-        children: [
-          Container(width: 300, height: 150,
-          decoration: ShapeDecoration(color: (0xFFF7FCFF)),)
-        ],
-      ),
-
-
     );
   }
-
 }
