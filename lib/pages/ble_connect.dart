ba17fbfc88333be83d95f0cf829103d22838837a
sabiliwafa29 <rosyadsiwa04@gmail.com>
2023-10-29 01:01:19 +0700

    revisi

--- lib/pages/ble_connect.dart
+++ lib/pages/ble_connect.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-
+void main() {
+  runApp(Ble_connect());
+}
 class Ble_connect extends StatelessWidget{
 
 
