2760054b8d239f18cf864cf89e737f0af53bed87
sabiliwafa29 <rosyadsiwa04@gmail.com>
2023-10-29 00:56:02 +0700

    ppp

--- lib/pages/ble_connect.dart
+++ lib/pages/ble_connect.dart
@@ -2,3 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 
+class Ble_connect extends StatelessWidget{
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BLE Connection'),
+        centerTitle: true,
+      ),
+      
+
+      body: Column(
+        children: [
+          Container(width: 300, height: 150,)
+        ],
+      ),
+
+
+    );
+  }
+
+}
