023c743ce1d038b31160be7b181b1406e62b7b96
Almira Firdaus Tsaani <1tsal0ftme@gmail.com>
2023-10-29 04:10:37 +0700

    subuh

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,41 +13,42 @@ class Home extends StatelessWidget {
           'Farmnesia', 
         ),
       ),
+
       //kolom box welcome
       body: Column(
-  crossAxisAlignment: CrossAxisAlignment.start, // Menyusun teks dari kiri ke kanan
-  children: [
-    Container(
-      width: 300,
-      height: 150,
-      margin: EdgeInsets.only(left: 40.0), // Mengatur margin kiri untuk teks pertama
-      padding: EdgeInsets.only(left: 20.0, right: 20.0), // Menambahkan padding pada teks pertama
-      child: Column( 
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start, // Menyusun teks dari kiri ke kanan
         children: [
-        Text(
-        'Welcome',
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 24,
-          fontFamily: 'Poppins',
-          fontWeight: FontWeight.w600,
-          height: 3,
-        ),
-      ),
-      SizedBox(height: 10.0),
-          Text(
-            'Application for Monitoring Goat and the cage',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 16,
-              fontFamily: 'Poppins',
-              fontWeight: FontWeight.w300,
-              height: 1.2,
+          Container(
+            width: 300,
+            height: 150,
+            margin: EdgeInsets.only(left: 40.0), // Mengatur margin kiri untuk teks pertama
+            padding: EdgeInsets.only(left: 20.0, right: 20.0), // Menambahkan padding pada teks pertama
+            child: Column( 
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              Text(
+              'Welcome',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 24,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w600,
+                height: 3,
+              ),
             ),
-          ),
-        ],
-      ),
+            SizedBox(height: 10.0),
+                Text(
+                  'Application for Monitoring Goat and the cage',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w300,
+                    height: 1.2,
+                  ),
+                ),
+              ],
+            ),
             decoration: ShapeDecoration(
                 color: Color(0xFFF7FCFF),
                 shape: RoundedRectangleBorder(
@@ -63,7 +64,7 @@ class Home extends StatelessWidget {
                 ],
             ),
           ),
-          
+
           //kolom box menu
           Column(
             children: [
@@ -81,102 +82,130 @@ class Home extends StatelessWidget {
                   ),
                 ),
               ),
-              //kolom tampilan amount
-              Column(
-                children: [
-                  Container(
-                    width: 300,
-                    height: 185,
-                    margin: EdgeInsets.only(left: 40.0), // Mengatur margin kiri untuk teks pertama
-                    padding: EdgeInsets.all(20.0), // Menambahkan padding pada teks pertama
-                    decoration: ShapeDecoration(
-                        gradient: LinearGradient(
-                            begin: Alignment(-0.94, -0.34),
-                            end: Alignment(0.94, 0.34),
-                            colors: [Color(0xFFA0D3E6), Color(0xFF3DA6D2)],
-                        ),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                        ),
-                    ),
-                  ),
 
-                Container(
-                    width: 300,
-                    height: 100,
-                    margin: EdgeInsets.only(left: 40.0, top: 20.0), // Mengatur margin kiri untuk teks pertama
-                    padding: EdgeInsets.all(20.0), // Menambahkan padding pada teks pertama
-                    decoration: ShapeDecoration(
+                //kolom tampilan cage biru
+                Column(
+                  children: [
+                    Container(
+                      width: 300,
+                      height: 185,
+                      margin: EdgeInsets.only(left: 40.0),
+                      padding: EdgeInsets.all(20.0),
+                      decoration: BoxDecoration(
                         gradient: LinearGradient(
-                            begin: Alignment(-1.00, -0.00),
-                            end: Alignment(1, 0),
-                            colors: [Color(0xFF50B6DC), Color(0xFFD8E8EE)],
+                          begin: Alignment(-0.94, -0.34),
+                          end: Alignment(0.94, 0.34),
+                          colors: [Color(0xFFA0D3E6), Color(0xFF3DA6D2)],
                         ),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                        ),
-                        shadows: [
-                            BoxShadow(
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color(0x3F000000),
+                            blurRadius: 4,
+                            offset: Offset(0, 4),
+                            spreadRadius: 0,
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    Column( //cage1
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 30,
+                          decoration: BoxDecoration(
+                            color: Color(0xFFEAE6E1),
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [
+                              BoxShadow(
                                 color: Color(0x3F000000),
                                 blurRadius: 4,
                                 offset: Offset(0, 4),
                                 spreadRadius: 0,
-                            )
-                        ],
-                    ),
-                ),
-
-
-
-                  Column(
-                    children: [
-                      SizedBox(height: 20), // Untuk memberikan sedikit jarak antara teks dan tombol
-                      InkWell(
-                        onTap: () {
-                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => Ble_connect(),
-                          ));
-                        },
-                        child: Container(
-                          width: 200,
-                          height: 50,
-                          margin: EdgeInsets.only(left: 40.0, top: 20.0), // Mengatur margin kiri untuk teks pertama
-                          child: Center(
-                            child: Text(
-                              'Connect to BLE',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
                               ),
-                            ),
+                            ],
                           ),
-                          decoration: ShapeDecoration(
-                            gradient: LinearGradient(
-                              begin: Alignment(-0.99, -0.13),
-                              end: Alignment(0.99, 0.13),
-                              colors: [Color(0xFF50B6DC), Color(0xFFC9E0EA)],
-                              ),
-                            shape: RoundedRectangleBorder(
+                        ),
+
+                        Column( //amount 
+                          children: [
+                            Container(
+                              width: 300,
+                              height: 85,
+                              margin: EdgeInsets.only(left: 40.0),
+                              padding: EdgeInsets.all(20.0),
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  begin: Alignment(-0.94, -0.34),
+                                  end: Alignment(0.94, 0.34),
+                                  colors: [Color(0xFFA0D3E6), Color(0xFF3DA6D2)],
+                                ),
                                 borderRadius: BorderRadius.circular(20),
-                            ),
-                            shadows: [
-                                BoxShadow(
+                                boxShadow: [
+                                  BoxShadow(
                                     color: Color(0x3F000000),
                                     blurRadius: 4,
                                     offset: Offset(0, 4),
                                     spreadRadius: 0,
-                                )
-                            ],
+                                  ),
+                                ],
+                              ),
+                            ),
+
+                            Column( //tombol next
+                              children: [
+                                SizedBox(height: 20), // Untuk memberikan sedikit jarak antara teks dan tombol
+                                InkWell(
+                                  onTap: () {
+                                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => Ble_connect(),
+                                    ));
+                                  },
+                                  child: Container(
+                                    width: 200,
+                                    height: 50,
+                                    margin: EdgeInsets.only(left: 40.0, top: 20.0), // Mengatur margin kiri untuk teks pertama
+                                    child: Center(
+                                      child: Text(
+                                        'Connect to BLE',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 18,
+                                        ),
+                                      ),
+                                    ),
+                                    decoration: ShapeDecoration(
+                                      gradient: LinearGradient(
+                                        begin: Alignment(-0.99, -0.13),
+                                        end: Alignment(0.99, 0.13),
+                                        colors: [Color(0xFF50B6DC), Color(0xFFC9E0EA)],
+                                        ),
+                                        shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                        ),
+                                      shadows: [
+                                        BoxShadow(
+                                          color: Color(0x3F000000),
+                                          blurRadius: 4,
+                                          offset: Offset(0, 4),
+                                          spreadRadius: 0,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
                         ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                      ],
+                    ),
+                  ],
+                ),
             ],
-          )
+          ),
         ],
-      ),
+      ), //return
     );
-  }
-}
+  } //build context
+} //home stateless
